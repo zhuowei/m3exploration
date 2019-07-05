5cc548adf319ad598857d2627490ac02eeeb5af0
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-07-05 03:04:02 -0700

    Load the image with m3inference as well

--- fakeme.py
+++ fakeme.py
@@ -6,16 +6,21 @@ import json
 # Load static data for one user
 with open("../m3webdemo/static/m3/xbox.json", "r") as infile:
     user_data = json.load(infile)
-dataset = m3inference.dataset.M3InferenceDataset([user_data["input"]], use_img=False) # we load our own image later
+user_data["input"]["img_path"] = "../m3webdemo/" + user_data["input"]["img_path"]
+
+# Load the image
+
+dataset = m3inference.dataset.M3InferenceDataset([user_data["input"]], use_img=True)
 dataset_tensors = list(dataset[0])
+start_image = dataset_tensors[-1].numpy()
 
 class M3ModifiedModel(m3inference.full_model.M3InferenceModel):
     def forward(self, data, label=None):
-        newdata = dataset_tensors + [data[0].float()]
+        newdata = dataset_tensors[:-1] + [data[0].float()]
         newdataloader = torch.utils.data.DataLoader(newdata, batch_size=1)
         output = super().forward(newdataloader, label)
         print(output)
-        return output[0]
+        return output[2] # non-corp/corp
 
 m3model = M3ModifiedModel(device="cpu")
 m3model.eval()
@@ -23,6 +28,6 @@ m3model.load_state_dict(torch.load("../m3webdemo/full_model.mdl", map_location="
 
 print("Loaded state")
 num_classes = 2 # corp/noncorp
-fmodel = foolbox.models.PyTorchModel(m3model, bounds=(0, 255), num_classes=num_classes)
-one = fmodel.forward_one(np.zeros((3, 224, 224)))
+fmodel = foolbox.models.PyTorchModel(m3model, bounds=(0, 1), num_classes=num_classes)
+one = fmodel.forward_one(start_image)
 print(one)
